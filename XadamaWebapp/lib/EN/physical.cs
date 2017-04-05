1d633da52610f3f08bbeaeee6a6e03f0916bc4fd
José Aldeguer <jas53@alu.ua.es>
2017-04-05 11:32:25 +0200

    Shop, Physical, Onlie (CAD/EN)

---
+++ XadamaWebapp/lib/EN/physical.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib.EN
+{
+    class Physical : Shop
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+
+        public Physical(string name, int id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+    }
+}
