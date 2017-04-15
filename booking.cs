8940d4e48a137d50fb2177ca1ed5733df00d5801
Mar <mbo11@alu.ua.es>
2017-04-15 17:09:41 +0200

    Update booking.cs

--- booking.cs
+++ booking.cs
@@ -8,7 +8,6 @@ namespace lib.EN
 {
     class Booking
     {
-<<<<<<< HEAD
         public String client { get; set; } 
         public String calendar { get; set; } 
         public int room { get; set; } 
@@ -16,23 +15,12 @@ namespace lib.EN
         public float price { get; set; }
 
         public Booking(String client, String calendar, int room, String hotel, float price)
-=======
-        public String client { get; set; } //FK + NNV
-        public String calendar { get; set; } //PK + FK
-        public int room { get; set; } // PK + FK
-        public String hotel { get; set; } //PK + FK
-
-        public Booking(String client, String calendar, int room, String hotel)
->>>>>>> origin/48792973Z
         {
             this.client = client;
             this.calendar = calendar;
             this.room = room;
             this.hotel = hotel;
-<<<<<<< HEAD
             this.price = price;
-=======
->>>>>>> origin/48792973Z
         }
 
         void Create(String dbname)
@@ -44,19 +32,12 @@ namespace lib.EN
         void Read(String dbname)
         {
             CAD.CADBooking b = new CAD.CADBooking(dbname);
-<<<<<<< HEAD
             Booking p = b.Read(calendar, room, hotel);
-=======
-            Booking p = b.Read(calendar, hotel, room);
->>>>>>> origin/48792973Z
             client = p.client;
             calendar = p.calendar;
             room = p.room;
             hotel = p.hotel;
-<<<<<<< HEAD
             price = p.price;
-=======
->>>>>>> origin/48792973Z
         }
 
         void Update(String dbname)
@@ -68,13 +49,7 @@ namespace lib.EN
         void Delete(String dbname)
         {
             CAD.CADBooking b = new CAD.CADBooking(dbname);
-<<<<<<< HEAD
             b.Delete(calendar, room, hotel);
         }
-=======
-            b.Delete(calendar, hotel, room);
-        }
-
->>>>>>> origin/48792973Z
     }
 }
