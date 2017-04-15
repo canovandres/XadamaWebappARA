91c1a8b58af223351a6c879343d87b4aba2377d7
Mar <mbo11@alu.ua.es>
2017-04-15 17:10:11 +0200

    Update cadbooking.cs

--- cadbooking.cs
+++ cadbooking.cs
@@ -13,14 +13,8 @@ namespace lib.CAD
         }
 
         public void Create(lib.EN.Booking b) { }
-<<<<<<< HEAD
         public lib.EN.Booking Read(String calendar, int room, String hotel) { }
         public void Update(lib.EN.Booking b) { }
         public void Delete(String calendar, int room, String hotel) { }
-=======
-        public lib.EN.Booking Read(String calendar, int num, String hotel) { }
-        public void Update(lib.EN.Booking b) { }
-        public void Delete(String calendar, int num, String hotel) { }
->>>>>>> origin/48792973Z
     }
 }
