29cfba094148caca821eeb9e5173a9e6305e05f5
Andrés <acc164@alu.ua.es>
2017-04-21 23:51:55 +0200

    Master page modified and some style

--- XadamaWebapp/XadamaWebapp/main.Master.designer.cs
+++ XadamaWebapp/XadamaWebapp/main.Master.designer.cs
@@ -112,15 +112,6 @@ namespace XadamaWebapp {
         protected global::System.Web.UI.WebControls.Image Image8;
         
         /// <summary>
-        /// Control Label2.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label2;
-        
-        /// <summary>
         /// Control Image9.
         /// </summary>
         /// <remarks>
@@ -209,15 +200,6 @@ namespace XadamaWebapp {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label11;
-        
-        /// <summary>
-        /// Control Label3.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label3;
         
         /// <summary>
         /// Control Label4.
