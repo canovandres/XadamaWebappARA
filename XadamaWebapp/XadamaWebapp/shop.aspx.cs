3974e7d2dd90bf15cb11379097bb712aba52fea7
Mar <mbo11@alu.ua.es>
2017-04-29 23:14:02 +0200

    Shop and Product 2

---
+++ XadamaWebapp/XadamaWebapp/shop.aspx.cs
@@ -0,0 +1,36 @@
+<<<<<<< HEAD
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XadamaWebapp
+{
+    public partial class shop : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XadamaWebapp
+{
+    public partial class shop : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+>>>>>>> origin/48792973Z
+}
