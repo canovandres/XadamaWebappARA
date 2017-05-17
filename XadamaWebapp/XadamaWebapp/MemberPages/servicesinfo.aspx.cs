736960df3d31d8b65d032304733dde380d11a037
DIEGO GARCIA CUTILLAS <dgc57@alu.ua.es>
2017-05-17 03:27:27 +0200

    Changes made by error

---
+++ XadamaWebapp/XadamaWebapp/MemberPages/servicesinfo.aspx.cs
@@ -0,0 +1,54 @@
+﻿using lib.EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XadamaWebapp
+{
+    public partial class servicesinfo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Restaurant"]!=null)
+            {
+                Restaurant r = (Restaurant)Session["Restaurant"];
+                ServiceTitle.Text = r.name;
+                ServiceDescription.Text = r.description;
+                zone.Text = "Zone :" + r.zone;
+                image.ImageUrl = r.image;
+                Button.Visible = true;
+            }
+            else if (Session["Ride"]!=null)
+            {
+                Ride r = (Ride)Session["Ride"];
+                ServiceTitle.Text = r.name;
+                ServiceDescription.Text = r.description;
+                minheight.Text = "The minimum height for this ride is: " + r.minHeight+" m";
+                speed.Text = "This ride reaches a speed of " + r.speed + " km/h";
+                zone.Text = "Zone: " + r.zone;
+                image.ImageUrl = r.image;
+                speed.Visible = true;
+                minheight.Visible = true;
+            }
+            else if (Session["Show"]!=null)
+            {
+                Show s = (Show)Session["Show"];
+                ServiceTitle.Text = s.name;
+                ServiceDescription.Text=s.description;
+                image.ImageUrl = s.image;
+                zone.Text = "Zone: " + s.zone;
+            }
+            /*ServiceTitle.Text = Session["ServiceName"].ToString();
+            Seadragon.SourceUrl = "~/Media/Rides/Europe/EuropeanGreece.xml";
+            ServiceDescription.Text = Session["ServiceDescription"].ToString();*/
+        }
+
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("bookrestaurant.aspx");
+        }
+    }
+}
---
+++ XadamaWebapp/XadamaWebapp/MemberPages/servicesinfo.aspx.designer.cs
@@ -0,0 +1,87 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace XadamaWebapp {
+    
+    
+    public partial class servicesinfo {
+        
+        /// <summary>
+        /// Control ServiceTitle.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ServiceTitle;
+        
+        /// <summary>
+        /// Control zone.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label zone;
+        
+        /// <summary>
+        /// Control minheight.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label minheight;
+        
+        /// <summary>
+        /// Control speed.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label speed;
+        
+        /// <summary>
+        /// Control Seadragon.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::AjaxControlToolkit.Seadragon Seadragon;
+        
+        /// <summary>
+        /// Control ServiceDescription.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ServiceDescription;
+        
+        /// <summary>
+        /// Control Button.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button;
+        
+        /// <summary>
+        /// Control image.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image image;
+    }
+}
