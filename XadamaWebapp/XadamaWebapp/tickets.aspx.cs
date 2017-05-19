d7f9e39718cf53ed6c55be4e7b25f559ebfd6d2b
Mar <mbo11@alu.ua.es>
2017-05-19 07:54:14 +0200

    Tickets

--- XadamaWebapp/XadamaWebapp/tickets.aspx.cs
+++ XadamaWebapp/XadamaWebapp/tickets.aspx.cs
@@ -1,49 +1,74 @@
-﻿using lib.EN;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Data.Common;
-using System.Data.SqlClient;
-using System.Data.SqlTypes;
-using System.Configuration;
+using lib.EN;
+using System.Net.Mail;
+using System.Data;
 
 namespace XadamaWebapp
 {
     public partial class tickets : System.Web.UI.Page
     {
+        private DataSet bdvirtual = new DataSet();
+        private DataTable t = new DataTable();
         Ticket ticket;
-        private DataSet ds = new DataSet();
-        private DataTable dt = new DataTable();
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            ticket = new Ticket("");
-            ds = ticket.typePrice();
-            DataTable t = ds.Tables["tickettype"];
-            ListView1.DataSource = t;
-            ListView1.DataBind();
-        }
+            TicketsError.Visible = false;
+            TicketsCorrect.Visible = false;
+            PromoCode.CssClass = "";
 
-        protected void buyTicket(object sender, EventArgs e)
+            bdvirtual = Ticket.getTypes();
+            ListViewTickets.DataSource = bdvirtual;
+            ListViewTickets.DataBind();
+
+            if (Session["Ticket"] != null)
+            {
+                ticket = (Ticket)Session["Ticket"];
+                Session.Remove("Ticket");
+
+                date.Text = ticket.day;
+                Children.Text = ticket.child.ToString();
+                Adults.Text = ticket.adult.ToString();
+
+                checkPurchase();
+            }
+        }
+        protected void checkPurchase()
+        {
+            TicketsError.Visible = false;
+            TicketsCorrect.Visible = false;
+            
+            if ((ticket.child <= 0 && ticket.adult <= 0) || Convert.ToDateTime(ticket.day) < DateTime.Today) // no funciona como deberia whyyyy
+            {
+                Date2.Text = ticket.day;
+                Children2.Text = ticket.child.ToString();
+                Adults2.Text = ticket.adult.ToString();
+                //Promo2.Text = 
+                Price2.Text = Math.Round(ticket.totalPrice(), 2).ToString();
+                TicketsError.Visible = true;
+            }
+            else
+            {
+                TicketsCorrect.Visible = true;
+            }
+        }
+        
+        protected void OnClickBuyNow(object sender, EventArgs e)
         {
             string email = null;
-            if(Session["Client"] != null)
+            if (Session["Client"] != null)
             {
                 email = ((Client)Session["Client"]).email;
             }
-            int nadults = Convert.ToInt32(Adults.Text);
-            int nchildren = Convert.ToInt32(Children.Text);
-
-            ticket = new Ticket("", email, date.Text, 0, "", nadults, nchildren);
-            bool buy = ticket.buyTickets();
-
-            Session["tickets"] = ticket;
+            // las conversiones a enteros no funcionan, no se por que 
+                     
+            ticket = new Ticket(0, email, date.Text, 0, Convert.ToInt32(Adults.ToString()), Int32.Parse(Children.Text));
+            Session["ticket"] = ticket;
             Page_Load(sender, e);
         }
     }
